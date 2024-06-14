30d9af9a9677946af4bbe3148933fcf51cb95be4
Crafteurmax <74319465+Crafteurmax@users.noreply.github.com>
2024-06-14 16:05:20 +0200

    better if saved

--- src/Drunk.cpp
+++ src/Drunk.cpp
@@ -3,4 +3,6 @@
 void Drunk::update(const sf::Time& elapsedTime)
 {
 	currentAngle += angularVelocity * elapsedTime.asSeconds();
+
+	_position = movingCenter + sf::Vector2f(cos(currentAngle), sin(currentAngle))*radius;
 }
