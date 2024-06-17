459f7ec52389ac6c4a086a0c6dc01c71093470e4
Crafteurmax <74319465+Crafteurmax@users.noreply.github.com>
2024-06-17 22:06:27 +0200

    change bullet sprite

--- src/Projectile.cpp
+++ src/Projectile.cpp
@@ -5,7 +5,7 @@ Projectile::Projectile(const pugi::xml_node& node) : DrawableObject(node) {}
 
 Projectile::Projectile(float _lifetime, ProjectileType _projectileType, sf::Vector2f pos,
 	sf::Vector2f vit, sf::Vector2f acc, sf::Vector2f aco) :
-	DrawableObject("projectile", {160.f,160.f}, "chat.png"),
+	DrawableObject("projectile", {32.f,32.f}, "bullet.png"),
 	_lifetime(_lifetime), _projectileType(_projectileType),
 	vit(vit),acc(acc),aco(aco)
 {
