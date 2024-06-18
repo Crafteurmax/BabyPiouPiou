99c175d4ecef797d81446b9747456b6ce0b1bd2b
Crafteurmax <74319465+Crafteurmax@users.noreply.github.com>
2024-06-18 23:31:48 +0200

    set bullet for eache type

--- src/Projectile.cpp
+++ src/Projectile.cpp
@@ -16,7 +16,7 @@ deltaPos(sf::Vector2f{ node.attribute("delta_x").as_float(), node.attribute("del
 Projectile::Projectile(float lifetime, ProjectileType projectileType, sf::Vector2f pos,
 	sf::Vector2f vit, sf::Vector2f acc, sf::Vector2f aco) :
 
-	DrawableObject("projectile", {32.f,32.f}, "bullet.png", pos),
+	DrawableObject("projectile", {32.f,32.f}, "bulletPlayer.png", pos),
 	_projectileType(projectileType), _currentLifetime(lifetime),
 	currentVit(vit), currentAcc(acc), currentAco(aco)
 {
